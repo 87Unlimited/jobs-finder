922807ec4a4a726c15d5cf0796857b31da93db0c
Chun <bbb123jack@gmail.com>
2024-09-28 00:34:04 +0800

    index-test-update

--- app/index.js
+++ app/index.js
@@ -1,21 +1,51 @@
-import { PropsWithChildren } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
 
-export const CustomText = ({ children } = PropsWithChildren) => <Text>{children}</Text>;
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
 
-export default function HomeScreen() {
+const Home = () => {
+    const router = useRouter();
+
     return (
-        <View style={styles.container}>
-            <CustomText>Welcome!</CustomText>
-        </View>
-    );
+        <SafeAreaView style={{
+            flex: 1, backgroundColor: COLORS.
+                lightWhite
+        }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                    headerTitle: ""
+                }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium
+                    }}
+                >
+                    <Welcome
+
+                    />
+
+                    <Popularjobs />
+                    <Nearbyjobs />
+
+                </View>
+            </ScrollView>
+
+        </SafeAreaView>
+    )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+export default Home;
