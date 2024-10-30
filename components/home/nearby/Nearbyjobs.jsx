e3c3ee7300ec91d260b5b662863e356f35cbc0ff
Chun <bbb123jack@gmail.com>
2024-10-30 22:49:14 +0800

    nearby-jobs

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -33,7 +33,9 @@ const Nearbyjobs = () => {
         ) : (
           data?.map((job) => {
             <NearbyJobCard
-
+              job={job}
+              key={`nearby-job-${job?.job_id}`}
+              handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
             />
           })
         )}
