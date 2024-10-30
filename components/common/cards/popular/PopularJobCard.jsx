24c66f768a61f78d3ef25b348fb8b9675cf106b5
Chun <bbb123jack@gmail.com>
2024-10-30 23:08:13 +0800

    nearby-jobs-api-calling

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -27,7 +27,7 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
       <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
 
       <View style={styles.infoContainer}>
-        <Text style={styles.jobName(selectedJob, item)}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
           {item.job_title}
         </Text>
         <Text style={styles.location}>{item.job_country}</Text>
