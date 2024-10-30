be37900c6885840f25ad3c6cfb5c25c6310f4b20
Chun <bbb123jack@gmail.com>
2024-10-30 22:33:17 +0800

    popular-jobs-api-calling

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
 import { useRouter } from 'expo-router'
 
 import styles from './popularjobs.style'
 import { COLORS, SIZES } from '../../../constants';
-import PopularjobsCard from '../../common/cards/popular/PopularJobCard';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 import useFetch from '../../../hook/useFetch';
 
 const Popularjobs = () => {
@@ -12,11 +12,17 @@ const Popularjobs = () => {
 
   const { data, isLoading, error } = useFetch
     ('search', {
-      query: 'React developer',
-      num_pages: 1
+      query: "React developer",
+      num_pages: "1",
     })
 
-  console.log(data);
+  console.log("data: " + data);
+
+  const [selectedJob, setSelectedJob] = useState()
+
+  const handleCardPress = (item) => {
+
+  }
 
   return (
     <View style={styles.container}>
@@ -34,12 +40,17 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4, 5, 6, 7, 8]}
-            renderItem={({ item }) => (
-              <PopularjobsCard
-                item={item}
-              />
-            )}
+            data={data}
+            renderItem={({ item }) => {
+              console.log("item: ", item);
+              return (
+                < PopularJobCard
+                  item={item}
+                  selectedJob={selectedJob}
+                  handleCardPress={handleCardPress}
+                />
+              );
+            }}
             keyExtractor={(item, index) => index.toString()}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,44 +1,36 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-// import { RAPID_API_KEY } from '@env';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-// const rapidApiKey = RAPID_API_KEY;
-
-const useFetch = async (endpoint, query) => {
+const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const http = require('https');
-
-    const axios = require('axios');
-
     const options = {
-        method: 'GET',
-        url: 'https://jsearch.p.rapidapi.com/${endpoint}',
-        params: { ...query },
+        method: "GET",
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
-            'x-rapidapi-key': 'd1e4174d09msh51c21dcbb0950e5p165928jsnebc5e5180a67',
-            'x-rapidapi-host': 'jsearch.p.rapidapi.com'
+            "X-RapidAPI-Key": 'd1e4174d09msh51c21dcbb0950e5p165928jsnebc5e5180a67',
+            "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
         },
+        params: { ...query },
     };
 
     const fetchData = async () => {
         setIsLoading(true);
 
         try {
-            const response = await axios.request(options);
-            console.log(response.data);
+            const response = await axios(options);
 
             setData(response.data.data);
             setIsLoading(false);
         } catch (error) {
             setError(error);
-            console.error(error);
+            console.log(error)
         } finally {
             setIsLoading(false);
         }
-    }
+    };
 
     useEffect(() => {
         fetchData();
@@ -50,6 +42,6 @@ const useFetch = async (endpoint, query) => {
     }
 
     return { data, isLoading, error, refetch };
-}
+};
 
 export default useFetch;
